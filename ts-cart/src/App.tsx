99b2086132e0c6fc45adfc8a6a14e8f4c4f44645
Taeng <dyek72731@gmail.com>
2021-08-11 07:40:30 +0900

    TS 프로젝트세팅

--- ts-cart/src/App.tsx
+++ ts-cart/src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { Drawer, LinearProgress, Grid, Badge } from "@material-ui/core";
+import { AddShoppingCartIcon } from "@material-ui/icons";
+import { Wrapper } from "./App.styles";
+
+const getProducts
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">start</div>;
 }
 
 export default App;
