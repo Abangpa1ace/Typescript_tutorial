ec73f38e28c94835d2d25b5af0f582160751ea9f
Abangpa1ace <dyek72731@gmail.com>
2021-08-19 21:55:58 +0900

    quiz 클론중

--- ts-quiz/src/App.tsx
+++ ts-quiz/src/App.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import QuesCard from './components/QuesCard';
 
 function App() {
+  const getQuiz = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      <h1>React Quiz</h1>
+      <button className="start" onClick={getQuiz}>Start</button>
+      <p className="score">Score: </p>
+      <p className="loader">
+        Loading Questions...
+      </p>
+      <QuesCard />
+      <button className="next" onClick={nextQuestion}>Next Question</button>
+    </div>
 
   );
 }
---
+++ ts-quiz/src/components/QuesCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {
+  question: string;
+  answer: string[];
+}
+
+const QuesCard: React.FC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default QuesCard;
